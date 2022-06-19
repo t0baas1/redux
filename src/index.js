7ab57cf2461d786412a53701403137006459832e
Topi Kanninen <topi.kanninen@sci.fi>
2022-06-19 14:09:00 +0300

    Part 6.a

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import noteReducer from './reducers/noteReducer';
+
+const store = createStore(noteReducer)
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
